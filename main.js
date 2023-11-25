e630cbdc83939b638914972ae1c4f52f89768412
ibudnitski <igor.budnitski@gmail.com>
2023-11-25 19:28:04 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,46 @@
+/* • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
+• • • • • JavaScript необходимые основы для начинающих • • • • •
+• • • • • • • • • • • • практический курс • • • • • • • • • • •
+• • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
+• • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • */
+// ================== 0. Настройка окружения ===============
+/*
+- Установка редактора
+- Установка плагина liveServer
+- Моя тема Ayu
+- Создание проекта
+*/
+
+// =================== 1. Подключение ===================
+/*
+- index.html
+- файл main.js
+- тег <script src></script>
+- Запуск в liveServer
+- команда console.log для вывода информации в консоль браузера
+- комментарии
+*/
+
+// Output to the console
+// console.log("Hello from Igor");
+
+/* let userName = "Summeray";
+console.log(userName); */
+
+// let time = 14;
+
+/* if (time < 12) {
+    console.log("Good morning!")
+} else if ( time > 12 && time < 18){
+    console.log("Good afternoon!")
+}else{
+    console.log("Good evening!")
+} */
+
+// let result = time < 12 ? "Good morning" : time > 12 && time < 18 ? 
+// "Good afternoon" : "Good evening!";
+
+// console.log(result)
+
+
+
